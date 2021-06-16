26f28fb60128bf4660d1e7a95924597c6439fcc3
Megan Lukes@gmail.com <meglukes@gmail.com>
2021-06-16 17:30:47 -0400

    started CSS

--- ClientApp/src/App.jsx
+++ ClientApp/src/App.jsx
@@ -3,6 +3,16 @@ import './custom.scss'
 
 export function App() {
   return (
-    <div className="d-flex justify-content-center display-2">Hello, World!</div>
+    <main>
+      <div className="mainback">
+        <header>
+          <h1>BambooFinder.com</h1>
+          <p></p>
+        </header>
+        <button>Find the Best Bamboo for Me</button>
+        <button>Find a Bamboo Nursery Near Me</button>
+        <button>Nursery Owners</button>
+      </div>
+    </main>
   )
 }

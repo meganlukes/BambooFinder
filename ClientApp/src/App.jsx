5639148a2e6d743c0ab09e5b75088f4a70fc9376
Megan Lukes@gmail.com <meglukes@gmail.com>
2021-06-16 22:15:11 -0400

    Working on nursery page

--- ClientApp/src/App.jsx
+++ ClientApp/src/App.jsx
@@ -1,18 +1,65 @@
-import React from 'react'
+import React, { Component, useEffect, useState } from 'react'
+import { Link, Route, Switch, useParams } from 'react-router-dom'
+import axios from 'axios'
 import './custom.scss'
+import { FrontPage } from './pages/FrontPage'
 
 export function App() {
   return (
     <main>
-      <div>
-        <header>
-          <h1>BambooFinder.com</h1>
-          <p></p>
-        </header>
-        <button>Find the Best Bamboo for Me</button>
-        <button>Find a Bamboo Nursery Near Me</button>
-        <button>Nursery Owners</button>
-      </div>
+      <Switch>
+        <Route exact path="/">
+          <FrontPage />
+        </Route>
+        <Route path="/nursery-list">
+          <div>
+            <header>
+              <h2>Nurseries</h2>
+            </header>
+
+            <form>
+              <input type="text" placeholder="Zip Code" />
+              <button className="small">Click</button>
+            </form>
+            <div class="grid-container-n">
+              <div class="item1">
+                <span>Nursery Name</span>
+              </div>
+              <div class="item2">Location</div>
+              <div class="item3">
+                <span>Shipping</span>
+              </div>
+              <div class="item4">
+                <span>Distance</span>
+              </div>
+            </div>
+            <div class="grid-container-n">
+              <div class="item1">
+                <span>Wilson Bros Gardens</span>
+              </div>
+              <div class="item2">McDonough, GA</div>
+              <div class="item3">
+                <span>Yes</span>
+              </div>
+              <div class="item4">
+                <span>300 miles</span>
+              </div>
+            </div>
+            <div class="grid-container-n">
+              <div class="item1">
+                <span>Bamboo Plants Online</span>
+              </div>
+              <div class="item2">Labelle, FL</div>
+              <div class="item3">
+                <span>Yes</span>
+              </div>
+              <div class="item4">
+                <span>100 miles</span>
+              </div>
+            </div>
+          </div>
+        </Route>
+      </Switch>
     </main>
   )
 }
---
+++ ClientApp/src/pages/FrontPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export function FrontPage() {
+  return (
+    <div>
+      <header>
+        <h1>BambooFinder.com</h1>
+      </header>
+      <button className="big">Find the Best Bamboo for Me</button>
+      <button className="big">Find a Bamboo Nursery Near Me</button>
+      <button className="big">Nursery Owners</button>
+    </div>
+  )
+}

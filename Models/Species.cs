71e92964eac79577f9cc2f56f0838f413db85a4a
Megan Lukes@gmail.com <meglukes@gmail.com>
2021-06-18 11:53:17 -0400

    attempt

--- Models/Species.cs
+++ Models/Species.cs
@@ -5,9 +5,16 @@ namespace BambooFinder.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Info { get; set; }
-        public int Height { get; set; }
-        public bool GrowthHabit { get; set; }
-        public int Zone { get; set; }
-        public int Light { get; set; }
+        public int MinHeight { get; set; }
+        public int MaxHeight
+        {
+            get; set;
+        }
+        public bool Clumping { get; set; }
+        public int MinZone { get; set; }
+        public int MaxZone { get; set; }
+        public int MinLight { get; set; }
+        public int MaxLight { get; set; }
     }
 }
+// psql --file=Models/exampledata.sql BambooFinderDatabase

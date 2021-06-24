935ac55198b0480834482d403cfae0e6e33ff416
Megan Lukes@gmail.com <meglukes@gmail.com>
2021-06-24 12:18:44 -0400

    start bamboo species

--- ClientApp/src/App.jsx
+++ ClientApp/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component, useEffect } from 'react'
 import { Route, Switch, useParams } from 'react-router-dom'
 import axios from 'axios'
+import { Link, useHistory } from 'react-router-dom'
 import './custom.scss'
 import { FrontPage } from './pages/FrontPage'
 import { ListOfNurseries } from './pages/ListOfNurseries'
@@ -11,6 +12,62 @@ import { AddBamboo } from './pages/AddBamboo'
 import { NewNursery } from './pages/NewNursery'
 import { NurserySuccess } from './pages/NurserySuccess'
 import { SpeciesSuccess } from './pages/SpeciesSuccess'
+import bluechungii from './Images/bluechungii.jpg'
+
+function Species() {
+  return (
+    <>
+      <header>
+        <h2>
+          <Link to="/">BambooFinder.com</Link>
+        </h2>
+      </header>
+      <p className="name">
+        <i>Blue chungii </i>
+      </p>
+      <p className="name">&quot;Angel Blue Mist Ghost Bamboo&quot;</p>
+      <ol className="bambooList">
+        <li>
+          <img
+            className="mainpic"
+            src={bluechungii}
+            alt="Woman standing next to bamboo"
+          />
+        </li>
+        <li>
+          <table>
+            <tr>
+              <td>Mature Height</td>
+              <td>15ft-20ft</td>
+            </tr>
+            <tr>
+              <td>Growth Habit</td>
+              <td>Clumping</td>
+            </tr>
+            <tr>
+              <td>
+                <ol>
+                  <li>Hardiness Zone</li>
+                  <li>
+                    <a href="https://planthardiness.ars.usda.gov/PHZMWeb/">
+                      Find your zone
+                    </a>
+                  </li>
+                </ol>
+              </td>
+              <td>9-11</td>
+            </tr>
+            <tr>
+              <td>Light Requirements</td>
+              <td>Part Sun to Full Sun</td>
+            </tr>
+          </table>
+        </li>
+      </ol>
+      <div className="descBox">More Information</div>
+    </>
+  )
+}
 
 export function App() {
   return (
@@ -19,29 +76,33 @@ export function App() {
         <Route exact path="/">
           <FrontPage />
         </Route>
-        <Route path="/nursery-list">
+        <Route exact path="/nursery-list">
           <ListOfNurseries />
         </Route>
-        <Route path="/bamboo-list">
+        <Route exact path="/bamboo-list">
           <BambooPlantsMain />
         </Route>
-        <Route path="/nursery-owners">
+        <Route exact path="/nursery-owners">
           <NurseryOwnersMain />
         </Route>
-        <Route path="/new-nursery">
+        <Route exact path="/new-nursery">
           <NewNursery />
         </Route>
-        <Route path="/edit-nursery"></Route>
-        <Route path="/add-bamboo">
+        <Route exact path="/edit-nursery"></Route>
+        <Route exact path="/add-bamboo">
           <AddBamboo />
         </Route>
-        <Route path="/add-nursery-success">
+        <Route exact path="/add-nursery-success">
           <NurserySuccess />
         </Route>
-        <Route path="/add-species-success">
+        <Route exact path="/add-species-success">
           <SpeciesSuccess />
         </Route>
+        <Route exact path="/species/:id">
+          <Species />
+        </Route>
       </Switch>
+
       <ol className="bottom banner">
         <li className="horizontalspace">BambooFinder.com</li>
         <li className="horizontalspace">
